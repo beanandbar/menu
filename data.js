465f040e7813529d40ad0983fcdfed525cd59c48
karam karam <116016222+Karamkaram17@users.noreply.github.com>
2025-03-20 23:58:32 +0200

    Update By Karam Karam

--- data.js
+++ data.js
@@ -1,1 +1,80 @@
-const MENU_DATA = {};
+const MENU_DATA = {
+  "store": {
+    "sm": {
+      "email": null,
+      "whatsappUrl": null,
+      "location": null,
+      "locationTranslation": null,
+      "locationUrl": null,
+      "instagramUrl": null,
+      "facebookUrl": null
+    },
+    "_id": "67622479ee097d842fd9afd4",
+    "menuId": "66d751cc2dfd45c14807f7cf",
+    "currency": "$",
+    "languages": [],
+    "mainLang": null,
+    "phoneNumber": null,
+    "displayedPhoneNumber": null,
+    "storeLogo": null,
+    "storeName": "The Bean & Bar",
+    "aboutUs": null,
+    "aboutUsTranslation": null,
+    "createdAt": "2024-12-18T01:25:13.181Z",
+    "updatedAt": "2024-12-19T19:49:02.083Z",
+    "__v": 0,
+    "templateId": "beanandbar"
+  },
+  "categories": [],
+  "sub_categories": [
+    {
+      "_id": "676478aeae2975c5d0f40821",
+      "menuId": "66d751cc2dfd45c14807f7cf",
+      "label": "Coffee",
+      "order": 1734637710724,
+      "img": null,
+      "bgImg": null,
+      "hide": false,
+      "createdAt": "2024-12-19T19:49:02.319Z",
+      "updatedAt": "2024-12-19T19:49:02.319Z",
+      "__v": 0
+    },
+    {
+      "_id": "676478aeae2975c5d0f40823",
+      "menuId": "66d751cc2dfd45c14807f7cf",
+      "label": "Food",
+      "order": 1734637717083,
+      "img": null,
+      "bgImg": null,
+      "hide": false,
+      "createdAt": "2024-12-19T19:49:02.551Z",
+      "updatedAt": "2024-12-19T19:49:02.551Z",
+      "__v": 0
+    },
+    {
+      "_id": "676478aeae2975c5d0f40825",
+      "menuId": "66d751cc2dfd45c14807f7cf",
+      "label": "Bar 1",
+      "order": 1734637738090,
+      "img": null,
+      "bgImg": null,
+      "hide": false,
+      "createdAt": "2024-12-19T19:49:02.782Z",
+      "updatedAt": "2024-12-19T19:49:02.782Z",
+      "__v": 0
+    },
+    {
+      "_id": "676478afae2975c5d0f40827",
+      "menuId": "66d751cc2dfd45c14807f7cf",
+      "label": "Bar 2",
+      "order": 1734637742587,
+      "img": null,
+      "bgImg": null,
+      "hide": false,
+      "createdAt": "2024-12-19T19:49:03.021Z",
+      "updatedAt": "2024-12-19T19:49:03.021Z",
+      "__v": 0
+    }
+  ],
+  "items": []
+};
