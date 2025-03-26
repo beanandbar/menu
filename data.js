c1e429aa1f33abae2a26cea74ae5782ba03e4950
karam karam <116016222+Karamkaram17@users.noreply.github.com>
2025-03-26 10:21:14 -0700

    Update By Karam Karam

--- data.js
+++ data.js
@@ -2,12 +2,12 @@ const MENU_DATA = {
   "store": {
     "sm": {
       "email": null,
-      "whatsappUrl": null,
       "location": null,
       "locationTranslation": null,
       "locationUrl": null,
+      "facebookUrl": null,
       "instagramUrl": null,
-      "facebookUrl": null
+      "whatsappUrl": null
     },
     "_id": "67622479ee097d842fd9afd4",
     "menuId": "66d751cc2dfd45c14807f7cf",
@@ -16,13 +16,14 @@ const MENU_DATA = {
     "mainLang": null,
     "phoneNumber": null,
     "displayedPhoneNumber": null,
-    "storeLogo": null,
+    "storeLogo": "https://menus.blob.core.windows.net/shared/66d751cc2dfd45c14807f7cf/d67c6448-a3f1-4cf3-9d6c-d6626d9a4388.jpg",
     "storeName": "The Bean & Bar",
     "aboutUs": null,
     "aboutUsTranslation": null,
     "createdAt": "2024-12-18T01:25:13.181Z",
-    "updatedAt": "2025-03-20T22:00:34.781Z",
+    "updatedAt": "2025-03-26T17:20:24.707Z",
     "__v": 0,
+    "quote": null,
     "templateId": "beanandbar"
   },
   "categories": [],
@@ -33,10 +34,10 @@ const MENU_DATA = {
       "label": "Coffee",
       "order": 1734637710724,
       "img": null,
-      "bgImg": "https://jmrealestate.github.io/night-in-paradise/img/menu/1.jpg",
+      "bgImg": "https://menus.blob.core.windows.net/shared/66d751cc2dfd45c14807f7cf/15a8289f-e3e4-4c4e-b785-402ab3463894.jpg",
       "hide": false,
       "createdAt": "2024-12-19T19:49:02.319Z",
-      "updatedAt": "2025-03-20T22:00:35.063Z",
+      "updatedAt": "2025-03-26T17:20:24.984Z",
       "__v": 0
     },
     {
@@ -45,10 +46,10 @@ const MENU_DATA = {
       "label": "Food",
       "order": 1734637717083,
       "img": null,
-      "bgImg": "https://jmrealestate.github.io/night-in-paradise/img/menu/2.jpg",
+      "bgImg": "https://menus.blob.core.windows.net/shared/66d751cc2dfd45c14807f7cf/0a5eaa2f-a031-4a59-89f5-0d2bd9cec6bb.jpg",
       "hide": false,
       "createdAt": "2024-12-19T19:49:02.551Z",
-      "updatedAt": "2025-03-20T22:00:35.344Z",
+      "updatedAt": "2025-03-26T17:20:25.259Z",
       "__v": 0
     },
     {
@@ -57,10 +58,10 @@ const MENU_DATA = {
       "label": "Bar 1",
       "order": 1734637738090,
       "img": null,
-      "bgImg": "https://jmrealestate.github.io/night-in-paradise/img/menu/3.jpg",
+      "bgImg": "https://menus.blob.core.windows.net/shared/66d751cc2dfd45c14807f7cf/f310e7ad-a23d-496a-95de-a4b88ca123cb.jpg",
       "hide": false,
       "createdAt": "2024-12-19T19:49:02.782Z",
-      "updatedAt": "2025-03-20T22:00:35.618Z",
+      "updatedAt": "2025-03-26T17:20:25.535Z",
       "__v": 0
     },
     {
@@ -69,10 +70,10 @@ const MENU_DATA = {
       "label": "Bar 2",
       "order": 1734637742587,
       "img": null,
-      "bgImg": "https://jmrealestate.github.io/night-in-paradise/img/menu/4.jpg",
+      "bgImg": "https://menus.blob.core.windows.net/shared/66d751cc2dfd45c14807f7cf/1d035f89-940f-471a-9ebc-9d167e8b4626.jpg",
       "hide": false,
       "createdAt": "2024-12-19T19:49:03.021Z",
-      "updatedAt": "2025-03-20T22:00:35.892Z",
+      "updatedAt": "2025-03-26T17:20:25.810Z",
       "__v": 0
     }
   ],
